55a3c2e5f9b5c00785de542f5b310a8f9a4cdd55
Kajigga Dev <kajigga@gmail.com>
2018-05-31 09:27:59 -0600

    Add dfrotz binary and fix PEP noncompliant lines

--- app.py
+++ app.py
@@ -2,19 +2,22 @@ from flask import Flask, request, render_template, jsonify
 from read_last_command import send_command, restart_game
 app = Flask(__name__)
 
+
 @app.route('/')
 def main():
     return render_template('main.html')
+
 
 @app.route('/restart', methods=['PUT'])
 def restart():
     return jsonify(restart_game())
 
+
 @app.route('/send', methods=['POST'])
 def send():
-    
     command = request.get_json().get('command')
     return jsonify(send_command(command))
+
 
 if __name__ == '__main__':
     app.run()
--- read_last_command.py
+++ read_last_command.py
@@ -22,10 +22,10 @@ template = env.get_template('templates/commands.txt')
 
 
 def restart_game():
-  # Delete any saved game files, etc
-  if os.path.exists(save_filename):
-    os.remove(save_filename)
-  return send_command(None, restore=False)
+    # Delete any saved game files, etc
+    if os.path.exists(save_filename):
+        os.remove(save_filename)
+    return send_command(None, restore=False)
 
 
 top_line_pat = re.compile('^(?P<location>.+?) {3,}(?P<right_side>.*)$')
@@ -33,49 +33,60 @@ score_pat = re.compile('^Score: (?P<score>\d*)')
 
 
 def parse_output(output, overwritten=True):
-  back_pedal = -3
-  if not overwritten:
     back_pedal = -3
+    if not overwritten:
+        back_pedal = -3
 
-  #print(output.decode('cp1252'))
-  game_output = output.decode('cp1252').split('> >')
-  #print(len(game_output))
-  output = game_output[back_pedal].replace('\n. \n', '\r\n').strip()
-  lines = output.splitlines()
-  mm = top_line_pat.match(lines[0])
-  matches = mm.groupdict()
-  
-  # Get first line which is he location on the left and
-  # the score on the right. These pieces should be displayed differently in
-  # chat than in Frotz
+    # print(output.decode('cp1252'))
+    game_output = output.decode('cp1252').split('> >')
+    # print(len(game_output))
+    output = game_output[back_pedal].replace('\n. \n', '\r\n').strip()
+    lines = output.splitlines()
+    mm = top_line_pat.match(lines[0])
+    matches = mm.groupdict()
 
-  score = 0
-  score_match = score_pat.match(matches['right_side'])
-  if score_match:
-    score = score_match.groupdict().get('score')
+    # Get first line which is he location on the left and
+    # the score on the right. These pieces should be displayed differently in
+    # chat than in Frotz
 
-  data = dict(
-      location=matches['location'], 
-      right_side=matches['right_side'], 
-      text = '\r\n'.join(lines[1:]),
-      score=score)
-  return data
+    score = 0
+    score_match = score_pat.match(matches['right_side'])
+    if score_match:
+        score = score_match.groupdict().get('score')
+
+    data = dict(
+        location=matches['location'],
+        right_side=matches['right_side'],
+        text='\r\n'.join(lines[1:]),
+        score=score)
+    return data
 
 
 def send_command(command, restore=True):
 
-  if command: 
-      command = command.strip()
+    if command:
+        command = command.strip()
 
-  overwrite = os.path.exists(save_filename)
+    overwrite = os.path.exists(save_filename)
 
-  tout = template.render(command=command, save_filename=save_filename, restore=restore, overwrite=overwrite)
+    tout = template.render(
+        command=command,
+        save_filename=save_filename,
+        restore=restore,
+        overwrite=overwrite)
 
-  log.debug('tout %s', tout)
+    log.debug('tout %s', tout)
 
-  args = ['dfrotz','-x','-p','-S','0', './games/LostPig.z8']
-  p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    args = [
+        os.path.join(BASE, 'bin', 'dfrotz'),
+        '-x', '-p', '-S', '0',
+        os.path.join(BASE, 'games', 'LostPig.z8')]
+    p = subprocess.Popen(
+        args,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
 
-  stdout, stderr = p.communicate(tout.encode('cp1252'))
+    stdout, stderr = p.communicate(tout.encode('cp1252'))
 
-  return parse_output(stdout, overwritten=overwrite)
+    return parse_output(stdout, overwritten=overwrite)
